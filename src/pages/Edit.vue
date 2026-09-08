84e22a9854ada287e66e5b58420ac484cc7ce7fb
ya-masa <smile.iwa.65@gmail.com>
2026-09-08 23:05:15 +0900

    7

--- src/pages/Edit.vue
+++ src/pages/Edit.vue
@@ -76,8 +76,8 @@ const updateItem = async (formData) => {
       k1=formData.kingaku1
       k2=-formData.kingaku1
     }else if(formData.type==="expense"){  //支出
-      k1=formData.kingaku1
-      k2=-formData.kingaku1
+      k1=-formData.kingaku1
+      k2=formData.kingaku1
     }else{                         //振替
       k1=formData.kingaku1
       k2=formData.kingaku2
